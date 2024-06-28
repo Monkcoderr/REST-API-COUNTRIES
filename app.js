0c0dca73923e4e0cc74a299d49128e381e059fa6
spiritualcoderr@gmail.com <spiritualcoderr@gmail.com>
2024-06-28 16:57:10 +0530

    dynamic element

--- app.js
+++ app.js
@@ -1,1 +1,22 @@
-sdfd
+
+
+const container = document.querySelector('.countries-container')
+
+const card = document.createElement('a')
+card.classList.add('country-card')
+// const imge = document.createElement('img')
+// imge.src = "https://flagcdn.com/wf.svg"
+// card.append(imge)
+
+const cardHTMl = `<img src="https://flagcdn.com/wf.svg" alt="flag">
+                <div class="card-text">
+                    <h3 class="card-title">India</h3>
+                    <p><b>Population: </b>8100000</p>
+                    <p><b>Region: </b>Asia</p>
+                    <p><b>Capital: </b>Delhi</p>
+                </div>`
+
+                card.innerHTML = cardHTMl
+                container.append(card)
+                
+        
